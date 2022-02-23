ddb4127c1c620266a741afb0d4e9b77dba273292
mister33221 <68389273+mister33221@users.noreply.github.com>
2022-02-23 09:58:42 +0800

    [init]

---
+++ meetWildBackend/src/main/java/com/KAI/meetWildBackend/MeetWildBackendApplication.java
@@ -0,0 +1,13 @@
+package com.KAI.meetWildBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetWildBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetWildBackendApplication.class, args);
+	}
+
+}
---
+++ meetWildBackend/src/test/java/com/KAI/meetWildBackend/MeetWildBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KAI.meetWildBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetWildBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
