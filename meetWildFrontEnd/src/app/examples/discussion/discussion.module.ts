245eef4e0c223e65ec9af202055136ec161e629d
mister33221 <68389273+mister33221@users.noreply.github.com>
2022-03-01 22:58:26 +0800

    add component

---
+++ meetWildFrontEnd/src/app/examples/discussion/discussion.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DiscussionRoutingModule } from './discussion-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DiscussionRoutingModule
+  ]
+})
+export class DiscussionModule { }
